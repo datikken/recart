224402631aba8b934752f52ffa857567c213f00a
Никита Шадыбеков <datikken@Nikitas-MacBook-Air.local>
2020-12-26 05:36:44 +0300

    verify email

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -16,6 +17,7 @@ class User extends Authenticatable
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use MustVerifyEmail;
 
     /**
      * The attributes that are mass assignable.
