fad03b7671ab91afa900269a73e62b1636c79c7b
Никита Шадыбеков <datikken@Nikitas-MacBook-Air.local>
2020-12-26 03:51:27 +0300

    test

--- resources/js/Shared/Information/InformationItem.vue
+++ resources/js/Shared/Information/InformationItem.vue
@@ -34,7 +34,7 @@
         name: "InformationItem",
         props: ['inf'],
         mounted() {
-            console.warn(this.$props.inf, 'inf items 55')
+            console.warn(this.$props.inf, 'inf items 66')
         },
         methods: {
             switchClass(arr, className) {
