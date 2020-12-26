b45852c3854732b51aa9edc7ac7d32c980af4e33
Никита Шадыбеков <datikken@Nikitas-MacBook-Air.local>
2020-12-26 04:17:02 +0300

    test

--- bin/rename.sh
+++ bin/rename.sh
@@ -4,4 +4,8 @@ cd ..
 pwd
 COMPOSER_MEMORY_LIMIT=-1 composer-php7.4 update --lock
 ssh localhost -p 222 node -v
-ssh localhost -p 222 npm -v && ssh localhost -p 222 npm run prod
+ssh localhost -p 222 npm -v
+ssh localhost -p 222
+cd recart.me
+pwd
+npm run prod
