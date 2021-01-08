8cc3ecd0e802d63907eb01746385222339cadbc8
datikken <tikken23@gmail.com>
2021-01-08 17:11:13 +0300

    about

--- resources/js/Pages/About/About.vue
+++ resources/js/Pages/About/About.vue
@@ -1,32 +1,50 @@
 <template>
-  <div class="about_hero">
-    <div class="about_hero_wrap">
-      <div class="about_hero_wrap_item">
-        <img src="/images/about/about_banner.png" alt="about_hero_wrap_ite_img">
-        <div class="about_hero_wrap_item-top">
-          <p class="about_hero_wrap_item-top_text">
-            C 2009 года мы производим картриджи, фотобумагу и плёнку для принтеров,
-            многофункциональных устройств и факсов
-            <b>HP, Samsung, Canon, Xerox, Brother, Kyocera, Epson, Panasonic, Sharp и Lexmark.</b>
-          </p>
+  <Fragment>
+    <div class="about_hero">
+      <div class="about_hero_wrap">
+        <div class="about_hero_wrap_item">
+          <img src="/images/about/about_banner.png" alt="about_hero_wrap_ite_img">
+          <div class="about_hero_wrap_item-top">
+            <p class="about_hero_wrap_item-top_text">
+              C 2009 года мы производим картриджи, фотобумагу и плёнку для принтеров,
+              многофункциональных устройств и факсов
+              <b>HP, Samsung, Canon, Xerox, Brother, Kyocera, Epson, Panasonic, Sharp и Lexmark.</b>
+            </p>
+          </div>
+          <div class="about_hero_wrap_item-bottom">
+            <p class="about_hero_wrap_item-bottom_text">
+              В наших картриджах используются дорогие японские <br/>
+              американские, южно-корейские комплектующие. На заводе внедрена система контроля качества. Мы уверены в
+              своих картриджах и даём на них гарантию.
+              Мы будем рады, если вы выберете продукцию Т2.
+            </p>
+          </div>
         </div>
-        <div class="about_hero_wrap_item-bottom">
-          <p class="about_hero_wrap_item-bottom_text">
-            В наших картриджах используются дорогие японские <br/>
-            американские, южно-корейские комплектующие. На заводе внедрена система контроля качества. Мы уверены в своих картриджах и даём на них гарантию.
-            Мы будем рады, если вы выберете продукцию Т2.
-          </p>
+        <AboutContent/>
+      </div>
+    </div>
+    <div class="navigator">
+      <div class="navigator_wrap">
+        <div class="navigator_wrap_line">
+          <div class="descline">
+            <div class="descline_item"></div>
+          </div>
+          <div class="navigator_wrap_line_items">
+              <div class="navigator_wrap_line_items_item"
+                   @click="(evnt) => recountLine(evnt)"
+                   v-for="year in this.years">{{ year.year }}</div>
+          </div>
         </div>
       </div>
-      <AboutContent/>
     </div>
-  </div>
+  </Fragment>
 </template>
 
 <script>
 import MainLayout from '@/Layouts/MainLayout'
 import AboutContent from '@/Shared/Abouts/AboutContent'
 import {Fragment} from 'vue-fragment'
+import {mapState} from 'vuex';
 
 export default {
   name: "About",
@@ -35,35 +53,22 @@ export default {
     AboutContent,
     Fragment
   },
-  mounted() {
-    this._setListeners(this.$el)
-  },
+  computed: mapState(['years']),
   methods: {
-    _setListeners(nav) {
-      let els = nav.querySelectorAll('.navigator_wrap-line_items-item');
-      let line = nav.querySelector('.descline_item');
-      let headings = nav.querySelectorAll('.aboutc_year');
-      let contWraps = nav.querySelectorAll('.aboutc');
-      let ww = window.screen.width;
+    recountLine(evnt) {
+      let line = document.querySelector('.descline_item');
+      // let headings = document.querySelectorAll('.aboutc_year');
 
-      if (ww < 1000) {
-        contWraps[1].style.display = 'none';
-      }
+      let target = evnt.target;
+      let offset = target.offsetLeft;
+      let twidth = target.offsetWidth;
+      // let val = parseFloat(target.outerText);
+      let w = offset + twidth;
 
-      els.forEach((el) => {
-        el.addEventListener('click', function (e) {
-          let target = e.target;
-          let offset = target.offsetLeft;
-          let twidth = target.offsetWidth;
-          let val = parseFloat(target.outerText);
-          let w = offset + twidth;
+      line.style.width = `${w}px`;
 
-          line.style.width = `${w}px`;
-
-          headings[0].innerText = val;
-          headings[1].innerText = val - 1;
-        })
-      })
+      // headings[0].innerText = val;
+      // headings[1].innerText = val - 1;
     }
   }
 }
