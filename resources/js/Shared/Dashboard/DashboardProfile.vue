0f00fd4473bf9f6dca8df7a52bb9ddac7e50cd60
datikken <tikken23@gmail.com>
2021-01-10 18:45:54 +0300

    dfill fix

--- resources/js/Shared/Dashboard/DashboardProfile.vue
+++ resources/js/Shared/Dashboard/DashboardProfile.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="app">
+    <div>
         <div class="container">
             <!--UPLOAD-->
             <form enctype="multipart/form-data" novalidate v-if="isInitial || isSaving">
@@ -112,34 +112,3 @@
 
 
 </script>
-
-<style>
-    .dropbox {
-        outline: 2px dashed grey; /* the dash box */
-        outline-offset: -10px;
-        background: lightcyan;
-        color: dimgray;
-        padding: 10px 10px;
-        min-height: 200px; /* minimum height */
-        position: relative;
-        cursor: pointer;
-    }
-
-    .input-file {
-        opacity: 0; /* invisible but it's there! */
-        width: 100%;
-        height: 200px;
-        position: absolute;
-        cursor: pointer;
-    }
-
-    .dropbox:hover {
-        background: lightblue; /* when mouse over to the drop zone, change color */
-    }
-
-    .dropbox p {
-        font-size: 1.2em;
-        text-align: center;
-        padding: 50px 0;
-    }
-</style>
